34ae1cd77accb69ac643d9b4aaf6b941782791a0
Kristoffer Haugsbakk <haugsbakk@yahoo.no>
2019-10-10 23:54:22 +0200

    Solve task 1

--- ch1/slp-1.hs
+++ ch1/slp-1.hs
@@ -21,3 +21,23 @@ prog = CompoundStm (AssignStm "a"
                                  Minus (NumExp 1))])
                             (OpExp (NumExp 10) Times (IdExp "a"))))
          (PrintStm [IdExp "b"]))
+
+-- Task 1
+maxargs :: Stm -> Int
+maxargs (CompoundStm stm1 stm2) =
+  max (maxargs stm1) (maxargs stm2)
+maxargs (AssignStm _ exp) = maxhelper exp
+maxargs (PrintStm list)
+  = max (length list) (maximum (map maxhelper list))
+
+maxhelper :: Exp -> Int
+maxhelper (IdExp _) = 0
+maxhelper (NumExp _) = 0
+maxhelper (OpExp exp1 _ exp2)
+  = max (maxhelper exp1) (maxhelper exp2)
+maxhelper (EseqExp stm exp) = max (maxargs stm) (maxhelper exp)
+
+-- From the book:
+--
+--    “For example, `maxargs(prog)` is `2`.”
+testMaxargs = (maxargs prog) == 2
